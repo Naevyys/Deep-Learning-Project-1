690ab25c703e1bcfce84b987360c7a34da791f9a
Naevyys <siriremerk@gmail.com>
2022-04-13 12:45:01 +0200

    Todo draft in my sandbox

--- sandbox-iris.py
+++ sandbox-iris.py
@@ -10,6 +10,28 @@ import matplotlib.pyplot as plt
 # - Can we import torchvision?
 # - Can we use matplotlib for visualisations and plots?
 
+
+# TODO
+# - Normalization for training & standardization of output
+# - Decide on the code architecture
+#   - Model classes/subclasses
+#   - Training implementation
+#     - Check how to use GPU & fallback on CPU if GPU unavailable during training
+#     - Save training history for plots later!!
+#     - Gitignore folders with training history? Not sure...
+#   - Saving the model to file (a few lines I think)
+#   - Loading the model (1 line I think)
+# - Decide on and implement a baseline model
+# - Implement training
+# - Implement grid/random search & k-fold cross-validation
+# - Train and optimize baseline model, here are some ideas:
+#   - learning rate
+#   - Optimizer
+#   - Layers (number of layers, number of units, kernel size, stride, ...)
+#   - Activation functions
+#   - Loss function
+# - Implement more model architectures & optimize params as well
+
 datafolder = "data"
 train_data_file = datafolder + "/train_data.pkl"
 val_data_file = datafolder + "/val_data.pkl"
@@ -23,9 +45,9 @@ noisy_imgs, clean_imgs = torch.load(val_data_file)
 degrees = 180
 translate = (0.1, 0.1)
 scale = (0.9, 1.2)
-brightness = (0.5, 1)
-contrast = (0.3, 0.9)
-saturation = (0, 0.5)
+brightness = (0.9, 1)
+contrast = (0.7, 1)
+saturation = (0.5, 1)
 hue = (0, 0.4)
 
 iterator = DataIterator(noisy_imgs_1, noisy_imgs_2)  # No augmentation
